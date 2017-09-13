386c32382ec23b0c7e92bd2a5999de1e6af29a7a
wei <wwang2017@gmail.com>
2017-09-13 11:21:22 -0700

    Add MySQL Database

--- src/rpc/ItemHistory.java
+++ src/rpc/ItemHistory.java
@@ -3,6 +3,7 @@ package rpc;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,6 +15,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import db.DBConnection;
+import db.DBConnectionFactory;
+import entity.Item;
 
 /**
  * Servlet implementation class ItemHistory
@@ -21,47 +25,77 @@ import org.json.JSONObject;
 @WebServlet("/history")
 public class ItemHistory extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ItemHistory() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private DBConnection conn = DBConnectionFactory.getDBConnection();
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public ItemHistory() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+	    String userId = request.getParameter("user_id");
+	    Set<Item> items = conn.getFavoriteItems(userId);
+	    JSONArray array = new JSONArray();
+	    for (Item item : items) {
+	      JSONObject obj = item.toJSONObject();
+	      array.put(obj);
+	    }
+	    RpcHelper.writeJsonArray(response, array);
+	}
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		try {
 			JSONObject input = RpcHelper.readJsonObject(request);
-			if (input.has("user_id") && input.has("visited")) {
-				String userId = (String) input.get("user_id");
-				JSONArray array = (JSONArray) input.get("visited");
-				List<String> visitedEvents = new ArrayList<>();
-				for (int i = 0; i < array.length(); i++) {
-					String eventId = (String) array.get(i);
-					visitedEvents.add(eventId);
-				}
+			String userId = input.getString("user_id");
+			JSONArray array = (JSONArray) input.get("favorite");
 
-				RpcHelper.writeJsonObject(response,
-						new JSONObject().put("status", "OK"));
-			} else {
-				RpcHelper.writeJsonObject(response,
-						new JSONObject().put("status", "InvalidParameter"));
+			List<String> histories = new ArrayList<>();
+			for (int i = 0; i < array.length(); i++) {
+				String itemId = (String) array.get(i);
+				histories.add(itemId);
 			}
+			conn.setFavoriteItems(userId, histories);
+			RpcHelper.writeJsonObject(response, new JSONObject().put("result", "SUCCESS"));
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
 	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+	    try {
+	      JSONObject input = RpcHelper.readJsonObject(request);
+	      String userId = input.getString("user_id");
+	      JSONArray array = (JSONArray) input.get("favorite");
+	
+	      List<String> histories = new ArrayList<>();
+	      for (int i = 0; i < array.length(); i++) {
+	        String itemId = (String) array.get(i);
+	        histories.add(itemId);
+	      }
+	      conn.unsetFavoriteItems(userId, histories);
+	      RpcHelper.writeJsonObject(response, new JSONObject().put("result", "SUCCESS"));
+	    } catch (JSONException e) {
+	      e.printStackTrace();
+	    }
+	}
+
 }
--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -13,6 +13,22 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import db.DBConnection;
+import db.DBConnectionFactory;
+import entity.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import entity.Item;
 import external.ExternalAPI;
 import external.ExternalAPIFactory;
@@ -24,7 +40,8 @@ import external.ExternalAPIFactory;
 @WebServlet("/search")
 public class SearchItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private DBConnection conn = DBConnectionFactory.getDBConnection();
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -36,18 +53,17 @@ public class SearchItem extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-    protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-    	double lat = Double.parseDouble(request.getParameter("lat"));
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+    		throws ServletException, IOException {
+    	String userId = request.getParameter("user_id");
+		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
 		// Term can be empty or null.
 		String term = request.getParameter("term");
-		ExternalAPI externalAPI = ExternalAPIFactory.getExternalAPI();
-		List<Item> items = externalAPI.search(lat, lon, term);
+		List<Item> items = conn.searchItems(userId, lat, lon, term);
 		List<JSONObject> list = new ArrayList<>();
 		try {
 			for (Item item : items) {
-				// Add a thin version of item object
 				JSONObject obj = item.toJSONObject();
 				list.add(obj);
 			}
